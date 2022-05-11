5da1d31df895a695233f5e0b524b730770554457
Илья Жердецкий <zherdetski_ilya@mail.ru>
2022-05-11 15:47:04 +0300

    First commit

---
+++ Stroki/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            int a = 5;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
